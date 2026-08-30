4461233486a054162f130e18f12737280584d44c
LeandroZipitria <leandro.zipitria@gmail.com>
2026-08-30 17:17:46 -0300

    Add local spell checking

---
+++ src/cspell-lib.d.ts
@@ -0,0 +1,36 @@
+declare module 'cspell-lib' {
+  export interface CSpellDocumentLike {
+    uri: string;
+    text: string;
+    languageId?: string;
+    locale?: string;
+  }
+
+  export interface SpellCheckDocumentOptions {
+    generateSuggestions?: boolean;
+    noConfigSearch?: boolean;
+    allowCompoundWords?: boolean;
+  }
+
+  export interface SpellCheckDocumentSettings {
+    words?: string[];
+    suggestionsTimeout?: number;
+  }
+
+  export interface SpellCheckIssue {
+    offset: number;
+    length: number;
+    text: string;
+    suggestions?: string[];
+  }
+
+  export interface SpellCheckDocumentResult {
+    issues: SpellCheckIssue[];
+  }
+
+  export function spellCheckDocument(
+    document: CSpellDocumentLike,
+    options: SpellCheckDocumentOptions,
+    settings: SpellCheckDocumentSettings
+  ): Promise<SpellCheckDocumentResult>;
+}
